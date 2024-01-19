70038d4c5f5dbe1530344289d4990b86bf858ceb
Chandram-Dutta <chandramdutta2004@gmail.com>
2024-01-20 01:08:35 +0530

    feat: added status route

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"net/http"
 	"github.com/fauxriarty/SUZI-backend/cmd/auth"
 	"github.com/gin-gonic/gin"
 )
 
+func Status(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
 func main() {
 	router := gin.Default()
+	router.GET("/", Status)
 	router.GET("/user/:id", auth.GetUserByID)
 	router.POST("/login", auth.Login)
-	router.Run("localhost:8080")
+	http.ListenAndServe(":8080", router)
 }
