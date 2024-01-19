68ff513f9b63d5544939370b378f5d4c96e8831c
fauxriarty <aditya.chheda04@gmail.com>
2024-01-20 01:00:33 +0530

    basic auth api made, cleaned up

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -1,28 +1,51 @@
 package auth
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 type User struct {
-	Username  string `json:"username"`
-	AuthToken string `json:"authtoken"`
+	ID       string `json:"id"`
+	Username string `json:"username"`
 }
 
-var users = []User{}
-
-func getUserByID(c *gin.Context) {
-
+//have to connect to database
+var users = []User{
+	{ID: "22bds0179", Username: "Aditya"},
+	{ID: "22bee0014", Username: "Chandram"},
 }
 
-func login(c *gin.Context) {
-
+func GetUserByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, user := range users {
+		if user.ID == id {
+			c.JSON(http.StatusOK, user)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/user:id", getUserByID)
-	router.POST("/login", login)
+func Login(c *gin.Context) {
+	var newUser User
 
-	router.Run("localhost:8080")
+	if err := c.BindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Check if the user already exists
+	for _, user := range users {
+		if user.Username == newUser.Username {
+			c.JSON(http.StatusConflict, gin.H{"message": "username already exists"})
+			return
+		}
+	}
+
+	// Append the new user to the users slice
+	users = append(users, newUser)
+
+	c.JSON(http.StatusOK, gin.H{"message": "user registered successfully", "user": newUser})
 }
