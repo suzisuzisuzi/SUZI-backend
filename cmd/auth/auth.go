3bac35ed371e6494d3a39b2728bc2318f80bf776
fauxriarty <aditya.chheda04@gmail.com>
2024-01-20 02:19:19 +0530

    feat: connecting to db

--- cmd/auth/auth.go
+++ cmd/auth/auth.go
@@ -3,49 +3,28 @@ package auth
 import (
 	"net/http"
 
+	"github.com/fauxriarty/SUZI-backend/cmd/user"
+
+	"github.com/fauxriarty/SUZI-backend/cmd/db"
 	"github.com/gin-gonic/gin"
 )
 
-type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-}
-
-//have to connect to database
-var users = []User{
-	{ID: "22bds0179", Username: "Aditya"},
-	{ID: "22bee0014", Username: "Chandram"},
-}
-
 func GetUserByID(c *gin.Context) {
 	id := c.Param("id")
-	for _, user := range users {
-		if user.ID == id {
-			c.JSON(http.StatusOK, user)
-			return
-		}
+
+	var newUser user.User
+	db.DB.First(&newUser, id)
+
+	if newUser.ID != "" {
+		c.JSON(http.StatusOK, gin.H{"message": "user found", "user": newUser})
+		return
 	}
+
 	c.JSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
 
 func Login(c *gin.Context) {
-	var newUser User
-
-	if err := c.BindJSON(&newUser); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Check if the user already exists
-	for _, user := range users {
-		if user.Username == newUser.Username {
-			c.JSON(http.StatusConflict, gin.H{"message": "username already exists"})
-			return
-		}
-	}
-
-	// Append the new user to the users slice
-	users = append(users, newUser)
+	var newUser user.User
 
 	c.JSON(http.StatusOK, gin.H{"message": "user registered successfully", "user": newUser})
 }
---
+++ cmd/db/setup.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"github.com/fauxriarty/SUZI-backend/cmd/user"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=myuser password=mypassword dbname=mydatabase port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(user.User{})
+	if err != nil {
+		return
+	}
+	DB = db
+}
