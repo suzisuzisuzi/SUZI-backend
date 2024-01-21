cf87caba98bc56cb51d5e667a40bdbc36f91ba50
fauxriarty <aditya.chheda04@gmail.com>
2024-01-22 02:02:07 +0530

    feat: added state field to user struct

--- cmd/user/user.go
+++ cmd/user/user.go
@@ -4,4 +4,5 @@ type User struct {
 	// ID         uint   `json:"id" ` //id has to be an unsigned integer
 	Username   string `json:"username"`
 	FirebaseID string `json:"firebaseID" gorm:"primaryKey"`
+	State      string `json:"state"`
 }
