4692273c4afd59429dc8d94a5a5084c652cb062c
fauxriarty <aditya.chheda04@gmail.com>
2024-01-25 00:38:30 +0530

    feat: added routes to new datalog struct

---
+++ cmd/data/data.go
@@ -0,0 +1,14 @@
+package data
+
+import (
+	"time"
+)
+
+type Datalog struct {
+	Timestamp  time.Time `json:"timestamp"`
+	FirebaseID string    `json:"firebaseID" gorm:"index"`
+	Latitude   float64   `json:"latitude"`
+	Longitude  float64   `json:"longitude"`
+	Altitude   float64   `json:"altitude"`
+	Category   string    `json:"category"`
+}
---
+++ cmd/handlers/handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/fauxriarty/SUZI-backend/cmd/data"
+	"github.com/fauxriarty/SUZI-backend/cmd/db"
+	"github.com/gin-gonic/gin"
+)
+
+func LogEntry(c *gin.Context) {
+	firebaseID := c.Param("firebaseID") // this should match the parameter name in the route
+
+	if firebaseID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Firebase ID is required"})
+		return
+	}
+
+	var datalog data.Datalog
+	if err := c.ShouldBindJSON(&datalog); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	datalog.FirebaseID = firebaseID
+	db.DB.Create(&datalog)
+
+	c.JSON(http.StatusOK, datalog)
+}
+
+func GetHeatmapData(c *gin.Context) {
+	category := c.Param("Category")
+
+	if category == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Category is required"})
+		return
+	}
+
+	var datalogs []data.Datalog
+	db.DB.Where("category = ?", category).Find(&datalogs)
+
+	c.JSON(http.StatusOK, datalogs)
+}
